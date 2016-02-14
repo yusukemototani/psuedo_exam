036380db12cdbcd65ff23d58441d687c477a9557
yusuto <motoyu0616@gmail.com>
2016-02-14 15:13:43 +0000

    migration title,comment

--- db/migrate/20160213105349_create_memos.rb
+++ db/migrate/20160213105349_create_memos.rb
@@ -1,6 +1,8 @@
 class CreateMemos < ActiveRecord::Migration
   def change
     create_table :memos do |t|
+      t.string :title
+      t.string :comment
       t.timestamps null: false
     end
   end
