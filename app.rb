eb6edf1f11a3b41c4b3beaf464b0001184c3611c
yusuto <motoyu0616@gmail.com>
2016-02-14 17:43:19 +0000

    basic.function

--- app.rb
+++ app.rb
@@ -2,13 +2,23 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
-require './models'
+require './models.rb'
 
 before do
-    @my_name = '<あなたのアダ名に変更する>'
+    @my_name = 'poo'
     @categories = Category.all
 end
 
 get '/' do
+    @memos = Memo.all
     erb :index
+end
+
+post '/create' do
+    Memo.create({
+        title: params[:title],
+        comment: params[:comment]
+    })
+    
+    redirect '/'
 end
--- models.rb
+++ models.rb
@@ -1,1 +1,8 @@
 ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+class Memo < ActiveRecord::Base
+    
+end
+
+class Category < ActiveRecord::Base
+    
+end
